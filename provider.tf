aa6647f65646c8c29a6991de5da588a3e311f507
jason-leong-ihpc <jason-leong@ihpc.a-star.edu.sg>
2024-11-24 13:39:46 +0800

    terraform code for ec2

---
+++ provider.tf
@@ -0,0 +1,11 @@
+# provider.tf
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "ap-southeast-1"
+}
